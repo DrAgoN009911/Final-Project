f2232ee377865762ffbc034804945d685ca6bc08
DrAgoN009911 <47123052+DrAgoN009911@users.noreply.github.com>
2022-12-09 22:28:05 -0500

    (Just need particle system after button push)

--- Assets/ParticleSystems.cs
+++ Assets/ParticleSystems.cs
@@ -8,12 +8,22 @@ public class ParticleSystems : MonoBehaviour
 {
 
     ParticleSystem ps;
-    
+    ParticleSystem ps1;
+    ParticleSystem ps2;
+    ParticleSystem ps3;
+    ParticleSystem ps4;
+
 
 
     private void Start()
     {
         ps = GetComponent<ParticleSystem>();
+        ps1 = GetComponent<ParticleSystem>();
+        ps2 = GetComponent<ParticleSystem>();
+        ps3 = GetComponent<ParticleSystem>();
+        ps4 = GetComponent<ParticleSystem>();
+        
+
     }
 
     public void OnSpray(InputAction.CallbackContext context)
@@ -21,21 +31,23 @@ public class ParticleSystems : MonoBehaviour
         ps.Emit(50);
     }
 
-
-    /*public void Spray2(InputAction.CallbackContext context)
+    public void OnSpray2(InputAction.CallbackContext context)
     {
-        ps.Emit(100);
+        ps1.Emit(100);
     }
 
-    public void Spray3(InputAction.CallbackContext context)
+    public void OnSpray3(InputAction.CallbackContext context)
     {
-        ps.Emit(40);
+        ps2.Emit(40);
     }
-    public void Spray4(InputAction.CallbackContext context)
+    public void OnSpray4(InputAction.CallbackContext context)
     {
-        ps.Emit(90);
-    */
+        ps3.Emit(90);
+    }
+    public void OnSpray5(InputAction.CallbackContext context)
+    {
+        ps4.Emit(200);
+    }
 }
-
   
 
